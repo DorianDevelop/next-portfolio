58133a15b444650dc9218943ce9be44b697d380e
DorianDevelop <dorianf.develop@gmail.com>
2025-11-28 10:12:33 +0100

    Skill page version 1.0

--- components/sub-pages/skills.tsx
+++ components/sub-pages/skills.tsx
@@ -1,10 +1,166 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
 
 const Skills = () => {
+
     return (
         <section id="window-content">
             <div className="content">
-                <h1>Skills</h1>
+                <div className="skills-page-container py-2">
+                    <h2 className="text-xl text-center">Here are my different skills</h2>
+                    <p className="text-xs text-center italic mb-2">I do not pretend to master all of them, far from that.</p>
+                    <div className="skill-mastery">
+                        <p className="text-center">Level chart:</p>
+                        <p className="text-xs">
+                            Red flower : <span className="text-red-500 font-bold">Advanced</span> <br/>
+                            Blue flower : <span className="text-blue-500 font-bold">Intermediate</span> <br/>
+                            Yellow flower : <span className="text-yellow-500 font-bold">Beginner</span>
+                        </p>
+                    </div>
+                    <div className="skills-page-content flex flex-col gap-3">
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Front-End</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="special ">Next JS
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">React JS
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">Angular JS
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">Vue JS
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">JavaScript
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">TypeScript
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">HTML & CSS
+                                    <Image src="/icons/red-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">PHP
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">ASP.NET Core MVC
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Back-End</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="special">Next JS
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">ASP.NET Core WebAPI
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">ASP.NET Core MVC
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">Express JS
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="web">PHP
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Programming</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="prog">C
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">C++
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">C#
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">Python
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">Rust
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">Bash
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="prog">VBA
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Others</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="server">Docker
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="server">GitHub & GitLab
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="server">Caddy
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="server">Linux & Windows
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Social</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="others">Autonomous
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Rigorous
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Respectful
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Patient
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Proactive
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Communication
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Innovative
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Problem Solving
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="others">Intellectual Curiosity
+                                    <Image src="/icons/purple-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills-page-content-item">
+                            <h1 className="text-l underline mb-1">Language</h1>
+                            <ul className="flex flex-wrap gap-2">
+                                <li className="special">French
+                                    <Image src="/icons/red-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="lang">English
+                                    <Image src="/icons/blue-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                                <li className="lang">Spanish
+                                    <Image src="/icons/yellow-flower.svg" alt="flower-icon" width={24} height={24} className="flower-level"/>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
